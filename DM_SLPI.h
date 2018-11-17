1188e8d2af2cf825d7349f57766f359ba1a647b5
Bobynouu <44372416+Bobynouu@users.noreply.github.com>
2018-11-17 15:25:22 +0100

    Update DM_SLPI.h

--- DM_SLPI.h
+++ DM_SLPI.h
@@ -10,11 +10,12 @@
 
 using namespace Eigen;
 
-
-VectorXd SGS( Matrix<double,Dynamic,Dynamic> A , VectorXd b , VectorXd x , double eps , int n_ite_max );
+SparseVector<double> SGS( SparseMatrix<double> A , SparseVector<double> b , SparseVector<double> x , double eps , int n_ite_max );
 
 SparseVector<double> res_min( SparseMatrix<double> A , SparseVector<double> b , SparseVector<double> x , SparseVector<double> x0, double eps , int n_ite_max );
 
 SparseVector<double> grad_conj( SparseMatrix<double> A , SparseVector<double> b , SparseVector<double> x , SparseVector<double> x0, double eps , int n_ite_max );
 
 SparseMatrix<double> create_mat(const std::string name_file_read, bool sym);
+
+void saveSolution(int it , int n_iter , double residu);
